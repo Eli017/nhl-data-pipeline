79ab1be472cf5f6a4314bfea5c01246b3062d86d
Eli Sokeland <elisokeland@gmail.com>
2023-05-22 13:29:36 -0400

    Added insertion of Game

--- src/functions/databaseGameHandler/handler.ts
+++ src/functions/databaseGameHandler/handler.ts
@@ -15,12 +15,31 @@ export const databaseGame = async (event, context, callback) => {
     for (const record of event.Records) {
         const game = JSON.parse(record.body) as Game;
         try {
-            let results = await mysql.query('SELECT * FROM table');
-            // Run clean up function
+            const [rows] = await mysql.query(`SELECT * FROM game WHERE game_pk=${game.gamePk}`);
+
+            if (rows.length() == 0) {
+                // TODO: Find out how penalty minutes are kept in liveData
+                const result = await mysql.query(`
+                INSERT INTO game VALUES (${game.gamePk}, ${game.teams.home.team.id}, 
+                    ${game.teams.away.team.id}, 0)
+                `);
+
+                // Run clean up function
+                await mysql.end();
+            
+                // Return the results
+                return result;
+            };
+
             await mysql.end();
-        
-            // Return the results
-            return results;
+
+            const response: APIGatewayProxyResult = {
+                statusCode: 200,
+                body: JSON.stringify({
+                    message: 'Game already exists!',
+                }),
+            };
+            return response;
         } catch (error) {
             console.error(error);
         }
---
+++ src/types/Player.ts
@@ -0,0 +1,9 @@
+class Player {
+    public playerPK: String;
+    public name: String;
+    public age: number;
+    public position: String;
+    public currentTeamPK: String;
+};
+
+export default Player;
